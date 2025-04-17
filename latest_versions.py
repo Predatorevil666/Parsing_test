1515a297c569f41614d37a0d2665176b8f22c701
Alexander Batogov <predatorevil666@mail.ru>
2025-04-17 15:07:20 +0300

    Add second parser

---
+++ latest_versions.py
@@ -0,0 +1,49 @@
+import requests_cache
+from pprint import pprint
+from bs4 import BeautifulSoup
+import re
+
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+
+if __name__ == '__main__':
+    session = requests_cache.CachedSession()
+    response = session.get(MAIN_DOC_URL)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'lxml')
+    sidebar = soup.find('div', {'class': 'sphinxsidebarwrapper'})
+    ul_tags = sidebar.find_all('ul')
+    for ul in ul_tags:
+        if 'All versions' in ul.text:
+            a_tags = ul.find_all('a')
+            break
+    else:
+        raise Exception('Ничего не нашлось')
+
+
+    # Список для хранения результатов.
+    results = []
+    # Шаблон для поиска версии и статуса:
+    pattern = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
+    # Цикл для перебора тегов <a>, полученных ранее.
+    for a_tag in a_tags:
+        # Извлечение ссылки.
+        link = a_tag['href']
+        # Поиск паттерна в ссылке.
+        text_match = re.search(pattern, a_tag.text)
+        if text_match is not None:
+            # Если строка соответствует паттерну,
+            # переменным присываивается содержимое групп, начиная с первой.
+            version, status = text_match.groups()
+        else:
+            # Если строка не соответствует паттерну,
+            # первой переменной присваивается весь текст, второй — пустая строка.
+            version, status = a_tag.text, ''
+            # Добавление полученных переменных в список в виде кортежа.
+        results.append(
+            (link, version, status)
+        )
+
+        # Печать результата.
+    for row in results:
+        print(*row)
